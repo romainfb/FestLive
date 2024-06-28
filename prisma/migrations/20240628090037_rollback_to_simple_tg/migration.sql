417435e696d9a378c68b61da37c3ac1686576dd1
thibault <thibault.Garrigues@icloud.Com>
2024-06-28 11:01:35 +0200

    feat: Skeletons + images

---
+++ prisma/migrations/20240628090037_rollback_to_simple_tg/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `total_reviews` on the `TotalRate` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "TotalRate" DROP COLUMN "total_reviews";
